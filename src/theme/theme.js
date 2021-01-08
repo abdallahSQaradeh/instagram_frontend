917f3812552e76ea22952c7ffc1a130bf9f22aa5
abdallahSQaradeh <abdallahsqaradeh@gmail.com>
2021-01-08 22:58:59 +0200

    add theme for the sign-up

---
+++ src/theme/theme.js
@@ -0,0 +1,44 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: "#0094F4",
+    },
+    text: {
+      primary: "#000",
+      secondary: "#979797", // #DBDBDB
+    },
+  },
+  typography: {
+    fontFamily:
+      ' -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif',
+    h6: {
+      fontSize: "17px",
+      lineHeight: "20px",
+      fontWeight: 600,
+    },
+  },
+  overrides: {
+    MuiOutlinedInput: {
+      root: {
+        fontSize: "14px",
+        fontWeight: 400,
+        lineHeight: "16px",
+      },
+    },
+    MuiInputLabel: {
+      root: {
+        fontSize: "14px",
+        lineHeight: "18px",
+      },
+    },
+    MuiButton: {
+      contained: {
+        "& .Mui-disabled": {
+          backgroundColor: "#000",
+        },
+      },
+    },
+  },
+});
