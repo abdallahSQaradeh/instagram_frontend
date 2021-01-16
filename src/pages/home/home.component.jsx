6a06b4a9b5130f05830c7ae98bbd5790702aa341
abdallahSQaradeh <abdallahsqaradeh@gmail.com>
2021-01-16 23:32:30 +0200

    refactor home code and retrive post from redux

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -2,32 +2,33 @@ import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 
 import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 import style from "./home.module.css";
 import Post from "../../components/post/post.component";
-import river from "../../assets/rivver.jpg";
-import beard from "../../assets/beard.jpg";
 import Story from "../../components/story/story.component";
 import "./style.css";
 import MoreModal from "./modals/more.modal";
 import LikesModal from "./modals/likes.modal";
 import PrefSuggestion from "./pref-users-suggestion/pref-suggestion.component";
-import joe from "../../assets/joe.jpg";
+import PostUploaderModal from "../../components/post-uploader/postUploaderModal.component";
 
+const stories = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 export default function Home(props) {
   const [open, setOpen] = useState(false);
   const [likesModal, setLikesModal] = useState(false);
   const [hide, setHide] = useState(false);
-  const { hideFooter } = props;
+  const { hideFooter, setOpenPost, openPost } = props;
+  const posts = useSelector((state) => state.posts);
   useEffect(() => {
     hideFooter(true);
     window.addEventListener("resize", (e) => {
       window.innerWidth < 975 ? setHide(true) : setHide(false);
     });
+
     return () => {
       window.removeEventListener("resize", () => {});
     };
-  }, []);
-
+  }, [hideFooter]);
   return (
     <div className={style.home}>
       <div className={style.dashboard}>
@@ -40,22 +41,22 @@ export default function Home(props) {
             infinite={false}
             rows={1}
           >
-            <Story />
-            <Story />
-            <Story />
-            <Story />
-            <Story />
-            <Story />
-            <Story />
-            <Story />
-            <Story />
-            <Story />
+            {stories.map((_) => (
+              <Story key={_} />
+            ))}
           </Slider>
         </div>
-        <Post setMore={setOpen} src={river} setLikes={setLikesModal} />
-        <Post src={beard} />
-        <Post src={river} />
-        <Post src={joe} />
+        {posts.map((post, idx) => {
+          return (
+            <Post
+              setMore={setOpen}
+              src={post.src}
+              caption={post.caption}
+              setLikes={setLikesModal}
+              key={`${post.id}${`xc${idx}`}`}
+            />
+          );
+        })}
       </div>
       {!hide ? (
         <div className={style.suggestionContainer}>
@@ -68,9 +69,12 @@ export default function Home(props) {
       {likesModal ? (
         <LikesModal setLikesModal={setLikesModal} open={likesModal} />
       ) : null}
+      {openPost ? <PostUploaderModal setOpenPost={setOpenPost} /> : null}
     </div>
   );
 }
 Home.propTypes = {
   hideFooter: PropTypes.func.isRequired,
+  setOpenPost: PropTypes.bool.isRequired,
+  openPost: PropTypes.bool.isRequired,
 };
